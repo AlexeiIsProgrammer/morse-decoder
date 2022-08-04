8f47ebd505ed50c9a93cdb59c8d1027257d99b9b
Alexei <avenge.now.r@mail.ru>
2022-08-04 11:35:51 +0300

    Edit: Complete Task!

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+
+    for (let i = 0; i < expr.length; i+=10) {
+        array.push(expr.slice(i, i+10));
+    }
+
+    let str = '';
+    let text = '';
+    let arrayOfDotesDashes = [];
+
+    for(let i = 0; i < array.length; i++) {
+        str = '';
+        for(let j = 0; j < array[i].length; j+=2) {
+            if(array[i].slice(j, j+2) == '11'){
+                str = `${str}-`;
+            }
+            else if (array[i].slice(j, j+2) == '10'){
+                str = `${str}.`;
+            }
+        }   
+        if(array[i] === '**********') {
+            text = `${text} `;
+        }
+        else{
+            text = `${text}${MORSE_TABLE[str]}`;
+        }
+        
+    }
+
+    return text.trim();
 }
 
 module.exports = {
